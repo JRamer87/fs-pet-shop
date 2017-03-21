2abd139ff2f946ba7fc95d2b72c49372913ef913
Jon Ramer <jramer87@gmail.com>
2017-03-21 00:33:33 -0500

    destroy works

--- pets.js
+++ pets.js
@@ -82,3 +82,60 @@ function createPet() {
     }
 }
 createPet();
+
+//UPDATES PET INFO AT A SPECIFIC INDEX
+function updatePet() {
+    if (method === "update") {
+        if (Number.isNaN(age) || !index || !age || !kind || !name || process.argv.length <= 2) {
+            console.error("Usage: node pets.js create AGE KIND NAME");
+            process.exitCode = 1;
+        } else if (method = "update" && index && age && kind && name) {
+            fs.readFile('pets.json', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                var pets = JSON.parse(data);
+                pets[index].age = Number(process.argv[3]);
+                pets[index].kind = process.argv[4];
+                pets[index].name = process.argv[5];
+
+                var petsJSON = JSON.stringify(pets);
+
+                fs.writeFile('pets.json', petsJSON, (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(pets[index]);
+                });
+            });
+        }
+    }
+}
+updatePet();
+
+function destroyPet() {
+    if (method === "destroy") {
+        if (!index || process.argv.length <= 2) {
+            console.error("Usage: node pets.js create AGE KIND NAME");
+            process.exitCode = 1;
+        } else if (method = "destroy" && index) {
+            fs.readFile('pets.json', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                var pets = JSON.parse(data);
+                console.log(pets[index]);
+                pets.splice(index, 1);
+
+                var petsJSON = JSON.stringify(pets);
+
+                fs.writeFile('pets.json', petsJSON, (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
+        }
+    }
+}
+destroyPet();
